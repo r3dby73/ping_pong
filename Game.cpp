4dbfc1d098e2c12d682bdc403fa0fb96187e8a3e
r3dby73 <bunnypart@protonmail.com>
2022-09-21 15:00:58 +0300

    Realize Game class run function

--- Game.cpp
+++ Game.cpp
@@ -106,3 +106,11 @@ Game::Game()
 	:mapWidth{ 60 }, mapHeight{ 20 }, ball{ new Ball(mapWidth / 2, mapHeight / 2) }, 
 	player1{ new Player(2, mapHeight / 2 - 2) }, player2{ new Player(mapWidth - 3, mapHeight / 2 - 2) }, gameOver{ false }
 {}
+
+void Game::run()
+{
+	while (!gameOver)
+	{
+		drawMap();
+	}
+}
