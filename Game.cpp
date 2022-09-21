514d562f862e652161a967cacbd054ae92d9217b
r3dby73 <bunnypart@protonmail.com>
2022-09-21 15:14:33 +0300

    Realize Game class resetGame function

--- Game.cpp
+++ Game.cpp
@@ -190,6 +190,13 @@ void Game::collisionLogic()
 	}
 }
 
+inline void Game::resetGame()
+{
+	ball->reset();
+	player1->reset();
+	player2->reset();
+}
+
 Game::Game()
 	:mapWidth{ 60 }, mapHeight{ 20 }, ball{ new Ball(mapWidth / 2, mapHeight / 2) }, 
 	player1{ new Player(2, mapHeight / 2 - 2) }, player2{ new Player(mapWidth - 3, mapHeight / 2 - 2) }, gameOver{ false }
